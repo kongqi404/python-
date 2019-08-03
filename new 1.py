9faa1c7d03c1a88a6cc16281bf62aec24e69ab38
kongqi <32472226+kongqiahaha@users.noreply.github.com>
2019-08-03 21:49:09 +0800

    random随机数

--- new 1.py
+++ new 1.py
@@ -1,9 +1,3 @@
-print ('i love you')
-temp = input("ccwxzsngsz?")
-guess = int(temp)
-if guess == 8:
-	print("wdnmd")
-	print("nb")
-else:
-	print("error")
-print("End")
+import random
+i = random.randint(0,10)
+print(i)
